824edd46fc3d85d052e82806a547e41184affa24
zawad17 <abdzawad00007@gmail.com>
2025-07-25 11:17:41 +0600

    python_mission_career

---
+++ python01.py
@@ -0,0 +1,27 @@
+# name = "I Live in a file that is called python01.py"
+# print (name + " and I am a Python file.")
+# user_input = input("What is your name? ")
+# print(f"hello {user_input}  welcome our family" )
+# print("my name is \"zawad\" " )
+# print(f'{type("10")} {type(10)}')   
+
+# list = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(f"List: {list}")
+# print(f"List[0]: {list[0]}")    
+# list_multiple = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     ["name", "age"]
+# ]
+
+student = {
+        "name": "Zawad",
+        "age" : 20,
+        "city": "Dhaka",
+        "favt_food" :["Biryani", "Pizza", "Burger"], 
+    }
+    
+
+access = student.get("name") ,student.get("favt_food")[0]
+
+print(access)
